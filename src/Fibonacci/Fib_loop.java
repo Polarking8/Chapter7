4e022d550670b4b3b5383b7799bdd726bf740d2a
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2022-01-27 11:56:36 -0700

    FibLoop done
    need to get tester, test, uml, and flowchart

--- src/Fibonacci/Fib_loop.java
+++ src/Fibonacci/Fib_loop.java
@@ -3,7 +3,15 @@ package Fibonacci;
 public class Fib_loop {
     public int findNthFibNumber(int n){
         int nth = 0;
-
+        int num1 = 1;
+        int num2 = 1;
+        int newNum = 0;
+        for(int i = 2; i<=n; i++){
+        newNum = num1+num2;
+        num2 = num1;
+        num1 = newNum;
+        }
+        nth = newNum;
         return nth;
     }
 }
